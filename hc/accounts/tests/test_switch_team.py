9736ef9ee93a65343e6a80bd6ecf7bbcbefb6ba6
clementm916 <clementm916@gmail.com>
2016-12-15 12:05:47 +0300

    corrects assertion for the right content

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -13,26 +13,21 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url, follow=True)
 
-        ### Assert the contents of 
-        self.assertIn(b'<!DOCTYPE html>\n<html lang="en">',r.content)
-        self.assertIn(b'<head>',r.content)
-        self.assertIn(b'<title>',r.content)
-        self.assertIn(b'<meta name="description" content="Monitor and Get Notified When Your Cron Jobs Fail. Free alternative to Cronitor and Dead Man\'s Snitch.">\n',r.content)
-
-
+        # Assert the contents of
+        self.assertContains(r, "This belongs to Alice")
 
     def test_it_checks_team_membership(self):
         self.client.login(username="charlie@example.org", password="password")
 
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url)
-        ### Assert the expected error code
-        self.assertEqual(r.status_code,403)
+        # Assert the expected error code
+        self.assertEqual(r.status_code, 403)
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
 
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url, follow=True)
-        ### Assert the expected error code
-        self.assertEqual(r.status_code,200)
+        # Assert the expected error code
+        self.assertEqual(r.status_code, 200)
